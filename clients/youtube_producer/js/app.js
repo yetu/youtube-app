f22966867257225071b92c81c814342866a3f362
Marcin Lagonda <marcin.lagonda@meelogic.com>
2015-05-12 12:39:27 +0200

    Adds routing parameters and Dashboard/View controllers with templates

--- clients/youtube_producer/js/app.js
+++ clients/youtube_producer/js/app.js
@@ -4,24 +4,34 @@ var youtubeApp = angular.module('youtubeApp',
 		'ngResource',
 		'pascalprecht.translate',
 		'reactTo',
+        require('./app_search').name,
+        require('./app_mode').name,
+        require('./app_sendToTv').name,
+        require('./ui_videoList').name,
 		require('./yt_result').name,
-		require('./yt_search').name,
+        require('./yt_search').name,
 		require('./yt_auth').name,
-		require('./yt_notification').name
+		require('./yt_notification').name,
+        require('./yt_viewer').name
 	]);
 
 youtubeApp.config(function ($routeProvider, $translateProvider, $httpProvider, $locationProvider, i18n) {
 	$httpProvider.defaults.useXDomain = true;
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
-	$locationProvider.html5Mode(true);
+	// $locationProvider.html5Mode(true);
 
 	$routeProvider
-		.when('/', {
-			template: require('./mainTemplate.html')
+		.when('/dashboard/:action?/:param?', {
+            controller: 'DashboardCtrl',
+			template: require('./dashboardTemplate.html')
+		})
+        .when('/view/:mode/:type/:id/:device?', {
+            controller: 'ViewerCtrl',
+			template: require('./viewerTemplate.html')
 		})
 		.otherwise({
-			redirectTo: '/'
+			redirectTo: '/dashboard'
 		});
 
 	//initialize the $translateProvider with all languages including their strings that are in i18n config file
@@ -32,7 +42,7 @@ youtubeApp.config(function ($routeProvider, $translateProvider, $httpProvider, $
     $translateProvider.preferredLanguage('en');
 });
 
-youtubeApp.run(function($location,$translate){
+youtubeApp.run(function($location, $translate){
     var params = $location.search();
     if(params.lang){
         $translate.use(params.lang);
@@ -75,3 +85,55 @@ youtubeApp.constant('i18n', {
         }
     }
 });
+
+// TODO: refactor to separated files
+// require('./dashboardController.js'); ???
+// require('./_controllers'); ???
+youtubeApp.controller('DashboardCtrl', ['$scope', 'ytSearchService', '$routeParams', '$location', 'appMode', '$rootScope', 
+  function($scope, ytSearchService, $routeParams, $location, appMode, $rootScope) {
+    // dummy init list
+    var dummyItem = [];
+    for(i = 0; i < 8; i++) {
+        dummyItem.push({ description: { text: 'To be implemented...'}});
+    }
+    $scope.mainResultList = [
+        { title: 'Category 1', items: [dummyItem[0], dummyItem[1], dummyItem[2], dummyItem[3]]},
+        { title: 'Category 2', items: [dummyItem[4], dummyItem[5], dummyItem[6], dummyItem[7]]}
+    ];
+
+    // temporary
+    $rootScope.appMode = appMode;
+
+    if($routeParams.action === 'search' && $routeParams.param) {
+        ytSearchService.getResult('search', $routeParams.param).then(function(data) {
+            $scope.mainResultList = data;
+            $scope.searchValue = $routeParams.param; // temporary as search inside
+        });
+    }
+
+    $scope.$on('app:search-value', function(event, query){
+        // temporary start search here
+        ytSearchService.getResult('search', query).then(function(data) {
+            $scope.mainResultList = data;
+            $scope.searchValue = $routeParams.param; // temporary as search inside
+        });
+        /* TODO: make path replace without reload
+        var action = '#/dashboard/search/' + query;
+        if(decodeURIComponent(window.location.hash) !== action) {
+            window.location = action; // replace with $location
+        }
+        */
+    });
+}]);
+
+youtubeApp.controller('ViewerCtrl', ['$scope', 'ytSearchService', 'appMode', '$rootScope', '$routeParams',
+  function($scope, ytSearchService, appMode, $rootScope, $routeParams) {
+
+    // temporary
+    $rootScope.temporaryMode = appMode.get() + 'mode';
+    $rootScope.temporaryType = $routeParams.mode;
+
+    $scope.$on('app:search-value', function(event, query){
+        // TODO: route to dashboard/search
+    });
+}]);
