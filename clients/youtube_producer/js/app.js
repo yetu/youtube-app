2e5f577021fa67fc1aa8b9dc327fc07e5fcaf9e7
Marcin Lagonda <marcin.lagonda@meelogic.com>
2015-05-13 23:45:27 +0200

    Adds duration filter

--- clients/youtube_producer/js/app.js
+++ clients/youtube_producer/js/app.js
@@ -86,6 +86,18 @@ youtubeApp.constant('i18n', {
     }
 });
 
+youtubeApp.filter('duration', function() {
+  return function(time) {
+        // http://stackoverflow.com/a/19094191
+        var array = (time || '').match(/(\d+)(?=[MHS])/ig) || [];
+        return array.map(function(item) {
+            if(item.length < 2) return '0' + item;
+            return item;
+        })
+        .join(':');
+  };
+});
+
 // TODO: refactor to separated files
 // require('./dashboardController.js'); ???
 // require('./_controllers'); ???
