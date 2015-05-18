ec8fbe3ae59e40b200a33f1bcdfee8f3fd241f32
Marcin Lagonda <marcin.lagonda@meelogic.com>
2015-05-18 08:03:50 +0200

    Moves internal youtube service config to feature folder

---
+++ clients/youtube_producer/js/yt_search/yt_youtubeServiceConfig.js
@@ -0,0 +1,34 @@
+/* global module */
+module.exports = ({
+    search: {
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        type: 'playlist,video',
+        part: 'snippet',
+        maxResults: config.maxSearchResults || 8
+    },
+    playlists: {
+        url: 'https://www.googleapis.com/youtube/v3/playlists',
+        part: 'snippet,contentDetails'
+    },
+    playlist: {
+        id: 'playlistId',
+        url: 'https://www.googleapis.com/youtube/v3/playlistItems',
+        part: 'snippet',
+        maxResults: 20
+    },
+    video: {
+        id: 'id',
+        url: 'https://www.googleapis.com/youtube/v3/videos',
+        part: 'snippet,contentDetails,statistics',
+        maxResults: 20
+    },
+    category: {
+        id: 'id',
+        url: 'https://www.googleapis.com/youtube/v3/videoCategories',
+        part: 'snippet',
+        maxResults: 20
+    },
+    regionCode: config.regionCode || 'GB',
+    relevanceLanguage: config.relevanceLanguage || 'en',
+    developerToken: config.youtubeDeveloperToken
+});
