d2a7b3a95cf8600351bfdc31da0f60eb39ce62d1
Son Binh Hoang <son.binh.hoang@meelogic.com>
2015-05-20 18:50:52 +0200

    - reduce video player's and result item's dimensions based on Esther's mockups (only for large screens min-width:1800px) - added assets to buttons sendToTv and show/hide playlist - modified gulp task 'copy-assets' to copy these assets into dist folders during build processes

--- clients/youtube_producer/gulp/active-tasks/copy-assets/copy-assets.js
+++ clients/youtube_producer/gulp/active-tasks/copy-assets/copy-assets.js
@@ -1,25 +1,31 @@
 var gulp = require('gulp'),
-	templates = require('../../common-config').path.templates,
-    fonts = require('../../common-config').path.fonts,
-    img = require('../../common-config').path.img,
-	views = require('../../common-config').path.views,
-	clean = require('gulp-clean');
+        templates = require('../../common-config').path.templates,
+        fonts = require('../../common-config').path.fonts,
+        img = require('../../common-config').path.img,
+        sendToTv = require('../../common-config').path.sendToTv,
+        yt_viewer = require('../../common-config').path.yt_viewer,
+        views = require('../../common-config').path.views,
+        clean = require('gulp-clean');
 
 gulp.task('clean-dev-dest', function () {
-	return gulp.src(cfg.dev.dest, {read: false})
-		.pipe(clean());
+    return gulp.src(cfg.dev.dest, {read: false})
+            .pipe(clean());
 });
 
 gulp.task('copy-assets', function () {
-	gulp.src(fonts.build.src, {base: './'})
-		.pipe(gulp.dest(fonts.build.dest));
-	gulp.src(templates.build.src, {base: './'})
-		.pipe(gulp.dest(templates.build.dest));
-	return gulp.src(img.build.src, {base: './'})
-		.pipe(gulp.dest(img.build.dest));
+    gulp.src(fonts.build.src, {base: './'})
+            .pipe(gulp.dest(fonts.build.dest));
+    gulp.src(templates.build.src, {base: './'})
+            .pipe(gulp.dest(templates.build.dest));
+    gulp.src(sendToTv.build.src, {base: './'})
+            .pipe(gulp.dest(sendToTv.build.dest));
+    gulp.src(yt_viewer.build.src, {base: './'})
+            .pipe(gulp.dest(yt_viewer.build.dest));
+    return gulp.src(img.build.src, {base: './'})
+            .pipe(gulp.dest(img.build.dest));
 });
 
-gulp.task('copy-ref-templates', function(){
-	return gulp.src(views.build.src, {base: './'})
-		.pipe(gulp.dest(views.build.dest));
+gulp.task('copy-ref-templates', function () {
+    return gulp.src(views.build.src, {base: './'})
+            .pipe(gulp.dest(views.build.dest));
 });
