d5641f3a56c712a597b0fda0ea9f10ce093839da
Marcin Lagonda <marcin.lagonda@meelogic.com>
2015-05-25 13:09:51 +0200

    Re-implements fetching categories First to local var, order by configured order and assigned once to the scope

--- clients/youtube_producer/js/_controllers/dashboardController.js
+++ clients/youtube_producer/js/_controllers/dashboardController.js
@@ -1,9 +1,10 @@
 /*jshint -W083 */
-/* global module */
+/* global module, config */
+
 /*
  * Dashbord controller
  */
-module.exports = (function($scope, ytYoutubeService, $routeParams, $location, appMode, $rootScope) {
+module.exports = (function($scope, ytYoutubeService, $routeParams, $location, appMode, $rootScope, $filter) {
     
     if($routeParams.action === 'search' && $routeParams.param) {
         ytYoutubeService.getResult('search', $routeParams.param).then(function(data) {
@@ -11,12 +12,25 @@ module.exports = (function($scope, ytYoutubeService, $routeParams, $location, ap
             $scope.searchValue = $routeParams.param; // temporary as search inside
         });
     } else {
+        var cid, list = [], order = [],
+            cat_no = config.dashboardCategories.length;
         // default categories view
         $scope.mainResultList = [];
         // TODO: handle no categories defined
-        for(var i = 0; i < config.dashboardCategories.length; i++) {
-            ytYoutubeService.getResult('popular', config.dashboardCategories[i].id, 4).then(function(data) {
-                $scope.mainResultList.push(data);
+        for(var i = 0; i < cat_no; i++) {
+            cid = config.dashboardCategories[i].id;
+            order.push(cid);
+            ytYoutubeService.getResult('popular', cid).then(function(data) {
+                data.order = order.indexOf(data.categoryId);
+                // overwrite youtube category name with configured one
+                data.title = config.dashboardCategories[data.order].name;
+                list.push(data);
+                if(cat_no === list.length) {
+                    // TODO: decide if defined order of categories is more important than loading time
+                    // in this case sort and assigng at once after load all
+                    $filter('orderBy')(list, 'order');
+                    $scope.mainResultList = list;
+                }
             });
         }
         // TODO: seems always the same videos - should be rerquested more and shuffle?
