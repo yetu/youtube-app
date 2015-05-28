984fdc9f5265b09dc6af02492e10c4311fbb4904
Marcin Lagonda <marcin.lagonda@meelogic.com>
2015-05-28 22:11:23 +0200

    Separates play icon to separated directive

--- clients/youtube_producer/js/ui_videoList/index.js
+++ clients/youtube_producer/js/ui_videoList/index.js
@@ -1,4 +1,5 @@
 module.exports = angular.module('ui_videoList', ['ngResource', 'pascalprecht.translate'])
 	.directive('uiVideoList', require('./ui_videoListDirective'))
-	.directive('uiVideoListItem', require('./ui_videoListItemDirective'));
+	.directive('uiVideoListItem', require('./ui_videoListItemDirective'))
+    .directive('uiVideoListPlayArrow', require('./ui_videoListPlayArrowDirective'));
 
