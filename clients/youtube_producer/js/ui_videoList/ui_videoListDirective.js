53d9791d17c64dbab7a1a68f854bc5b60694e787
Marcin Lagonda <marcin.lagonda@meelogic.com>
2015-06-01 08:02:30 +0200

    Removes ngInfinitiveScroll; adds own simple scroll detection

--- clients/youtube_producer/js/ui_videoList/ui_videoListDirective.js
+++ clients/youtube_producer/js/ui_videoList/ui_videoListDirective.js
@@ -1,5 +1,5 @@
 /*
- * <ui-video-list ng-model="" display="floating" control="pc" play-link="" service=""></ui-video-list>
+ * <ui-video-list ng-model="" display="floating" control="pc" play-link="" service="" load-more=""></ui-video-list>
  *
  * @attr ng-model array Scope model to be used as data feed - model containing: { etag, next, items }
  *                      where etag and next are part of data provided by service and used for getNext method,
@@ -8,7 +8,8 @@
  * @attr display string Display type - 'horizontal', 'list' or 'floating' (default) for styling and controls behaviour
  * @attr control string Control style - 'tv' or 'pc' (default) - used for control and reacting on events @todo
  * @attr play-link string Link pattern to open video - will replace :attribute if found in element item properties (e.g. '#/show/:type/:id')
- * @attr service string Link to service to open the video with
+ * @attr service string Name of service with "load more" functionality (getNext method) - used by load-more functionality
+ * @attr load-more string 'button' - indicates if button "load more" should be appended; 'scroll' - indicates if "load more" should be called on scroll bottom
  */
 module.exports = function () {
     return {
@@ -19,32 +20,37 @@ module.exports = function () {
             playLink: '@playLink',
             playFn: '@playFn', // TODO: some function binding?
             displayType: '@display',
-            controlType: '@control'
+            controlType: '@control',
+            loadMore: '@?loadMore'
         },
-        controller: function ($scope, $element, $attrs, $injector, $location) {
+        controller: function ($scope, $element, $attrs, $injector) {
             var myService;
 
             if ($attrs.service) {
                 myService = $injector.get($attrs.service);
             }
 
-            //TODO: currently displayType is not used for indicating which type of list is being shown. Workaround, basing on $location service.
-            $scope.showLoadMoreButton = ($location.$$path === '/dashboard/') || ($location.$$path === '/dashboard')? true: false;
-            $scope.loadMore = function(){
+            $scope.loadNext = function() {
+                if(!$scope.videoList.etag || !$scope.videoList.next || $scope.loadingMore) {
+                    return;
+                }
+
+                $scope.loadingMore = true;
+
                 myService.getNext($scope.videoList.etag, $scope.videoList.next).then(
                     function(moreVideos){
                         var temparray = $scope.videoList.items;
                         $scope.videoList.items = temparray.concat(moreVideos.items);
                         $scope.videoList.next = moreVideos.next;
-                    }
-                );
+                    })
+                    .finally(function() {
+                        $scope.loadingMore = false;
+                    });
             };
 
         },
         link: function (scope, element) {
-
-            console.error('ui-video-directive scope: ', scope.videoList);
-
+            // default display type
             if (!scope.displayType) {
                 scope.displayType = 'floating';
             }
@@ -56,6 +62,19 @@ module.exports = function () {
                     console.error('ui-video-directive: ' + scope.playFn + ' is not a function');
                 }
             };
+
+            if(scope.loadMore === 'scroll') {
+                // TODO: check in unbind necessary on $destroy
+                element.bind('scroll', function () {
+                    var elem = element[0],
+                        distance = elem.scrollHeight * 0.1, // 10% to end
+                        toBottom = elem.scrollHeight - elem.clientHeight - elem.scrollTop;
+                    
+                    if(toBottom < distance) {
+                        scope.loadNext();
+                    }
+                });
+            }
         }
     };
 };
