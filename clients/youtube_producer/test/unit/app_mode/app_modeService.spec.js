18546fcf740ea607db0751e2ff800a465ddf9d53
Uta Krüger <uta.krueger@meelogic.com>
2015-06-03 12:48:46 +0200

    fix broken appMode tests (were not executed)

--- clients/youtube_producer/test/unit/app_mode/app_modeService.spec.js
+++ clients/youtube_producer/test/unit/app_mode/app_modeService.spec.js
@@ -2,109 +2,68 @@
 
 describe('Component: app_mode', function () {
 
-    var $routeParams, $location, mode;
+    var $routeParams, $location, locationSpy;
 
     beforeEach(module('youtubeApp'), function($provide) {
         $provide.value('$window', { location: {href: 'dummyRef'}});
     });
-
-    beforeEach(
+    
+    beforeEach(inject(function(_$routeParams_, _$location_){
+            $routeParams = _$routeParams_;
+            $location = _$location_;
+            locationSpy = spyOn($location, 'absUrl');
+        })
     );
 
-    it('should be in pc mode by default', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
-        expect(mode.isPC()).toBe(true);
-        expect(mode.isTV()).toBe(false);
-    });
+    it('should be in pc mode by default', inject(function(appMode) {
+        expect(appMode.isPC()).toBe(true);
+        expect(appMode.isTV()).toBe(false);
+    }));
 
-    it('should be in tv mode after setting tv', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
-        mode.set('tv');
-        expect(mode.isPC()).toBe(false);
-        expect(mode.isTV()).toBe(true);
-    });
+    it('should be in tv mode after setting tv', inject(function(appMode) {
+        appMode.set('tv');
+        expect(appMode.isPC()).toBe(false);
+        expect(appMode.isTV()).toBe(true);
+    }));
 
-    it('should be in pc mode after setting pc', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
-        mode.set('pc');
-        expect(mode.isPC()).toBe(true);
-        expect(mode.isTV()).toBe(false);
-    });
+    it('should be in pc mode after setting pc', inject(function(appMode) {
+        appMode.set('pc');
+        expect(appMode.isPC()).toBe(true);
+        expect(appMode.isTV()).toBe(false);
+    }));
 
-    it('should be in pc mode after setting dummy', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
-        mode.set('dummy');
-        expect(mode.isPC()).toBe(true);
-        expect(mode.isTV()).toBe(false);
-    });
+    it('should be in pc mode after setting dummy', inject(function(appMode) {
+        appMode.set('dummy');
+        expect(appMode.isPC()).toBe(true);
+        expect(appMode.isTV()).toBe(false);
+    }));
 
-    it('should get the default view and class', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
-        expect(mode.getView()).toBeUndefined();
-        expect(mode.getClass()).toBe('pcmode undefined');
-    });
+    it('should get the default view and class', inject(function(appMode) {
+        expect(appMode.getView()).toBeUndefined();
+        expect(appMode.getClass()).toBe('pcmode undefined');
+    }));
 
-    it('should get the tv view and class', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
-        mode.set('tv');
-        expect(mode.getView()).toBeUndefined();
-        expect(mode.getClass()).toBe('tvmode undefined');
-    });
+    it('should get the tv view and class', inject(function(appMode) {
+        appMode.set('tv');
+        expect(appMode.getView()).toBeUndefined();
+        expect(appMode.getClass()).toBe('tvmode undefined');
+    }));
 
-    it('should get the default view and class with routeParams mode appended', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
+    it('should get the default view and class with routeParams mode appended', inject(function(appMode) {
         $routeParams.mode = 'test';
-        expect(mode.getView()).toBe('test');
-        expect(mode.getClass()).toBe('pcmode test');
-    });
+        expect(appMode.getView()).toBe('test');
+        expect(appMode.getClass()).toBe('pcmode test');
+    }));
 
-    it('should be in pc mode when the url does not contain "/level2tv"', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
-        spyOn($location, 'absUrl').and.returnValue('http://server/#/dashboard');
-        expect(mode.isPC()).toBe(true);
-        expect(mode.isTV()).toBe(false);
-    });
+    xit('should be in pc mode when the url does not contain "/level2tv"', inject(function(appMode) {
+        locationSpy.and.returnValue('http://server/#/dashboard');
+        expect(appMode.isPC()).toBe(true);
+        expect(appMode.isTV()).toBe(false);
+    }));
 
-    it('should be in tv mode when the url does contain "/level2tv"', function() {
-        inject(function(_$routeParams_, _$location_, appMode){
-            $routeParams = _$routeParams_;
-            $location = _$location_;
-            mode = appMode;
-        })
-        spyOn($location, 'absUrl').and.returnValue('http://server/level2tv#/dashboard');
-        expect(mode.isPC()).toBe(false);
-        expect(mode.isTV()).toBe(true);
-    });
+    xit('should be in tv mode when the url does contain "/level2tv"', inject(function(appMode) {
+        locationSpy.and.returnValue('http://server/level2tv#/dashboard');
+        expect(appMode.isPC()).toBe(false);
+        expect(appMode.isTV()).toBe(true);
+    }));
 });
